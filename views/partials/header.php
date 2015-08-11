242b7184930927353367818e7bdcadc03a0c5730
Johnathan Christopherson <johnathan.christopherson@live.com>
2015-08-11 14:30:37 -0500

    pagination working in index.php

--- views/partials/header.php
+++ views/partials/header.php
@@ -5,7 +5,7 @@ require_once 'navbar.php';
 ?>
 
 <html>
-<header>
+<head>
 <style>
 .wow {
 
@@ -13,7 +13,7 @@ require_once 'navbar.php';
 }
 
 </style>
-</header>
+</head>
 
 <div class="wow">
 <img src="/img/WoW.jpg" width="100%" height="200" alt="logo">
