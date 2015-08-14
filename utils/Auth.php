286b10b946278170fb79b01887365ef73d4b9604
Johnathan Christopherson <johnathan.christopherson@live.com>
2015-08-14 09:46:55 -0500

    friday push

--- utils/Auth.php
+++ utils/Auth.php
@@ -1,68 +1,43 @@
 <?php
 
-// 	public static function attempt($username, $password)
-// 	{
-// 		$log = new Logger ();
-// 		if(escape(strtolower(Input::get('username'))) == "guest" && (password_verify($password, static::$password))) {
-// 			$_SESSION['LOGGED_IN_USER'] = true;	
-// 			$_SESSION['username'] = $username;
-// 			$log->info("User {$username} logged in.");
-// 		} else {
-// 			$log->error("User {$username} failed to log in!");	
-// 		}
-// 	}
-	
-// 	public static function check ()
-// 	{
-// 		if(isset($_SESSION['LOGGED_IN_USER'])) {
-// 			return true; 
-// 		} else {
-// 			return false;
-// 		}
-// 	}
-	
-// 	public static function user ()
-// 	{
-// 		if(isset($_SESSION['LOGGED_IN_USER'])) {
-// 			return $username;
-// 		} else {
-// 			header('location: login.php');
-// 			exit();
-// 		}
 
-
-// 	}
-
-// 	public static function logout ()
-// 	{
-// 		session_start();
-// 		$_SESSION = [];
-
-// 		if (ini_get("session.use_cookies")) {
-// 	        $params = session_get_cookie_params();
-// 	        setcookie(session_name(), '', time() - 42000,
-// 	            $params["path"], $params["domain"],
-// 	            $params["secure"], $params["httponly"]
-// 	        );
-// 	    }
-
-// 		session_destroy();
-// 	}
-		
-// }
-
-
-
-?>
-
-
-<html>
-<body>
-  <form action="login.php" method="POST">
-  Username: <input type="text" name="username" />
-  Password: <input type="password" name="password" />
-  <input type="submit" />
-  </form>
-</body>
-</html>
-
+class Auth
+{
+    public static function attempt($email, $password, $dbc)
+    {
+        $query = 'SELECT email, password FROM users WHERE email = :email;';
+        $stmt  = $dbc->prepare($query);
+        $stmt->bindValue(':email', $email, PDO::PARAM_STR);
+        $stmt->execute();
+        $results = $stmt->fetch(PDO::FETCH_ASSOC);
+        $userId  = $results['email'];
+        $passwordHash = $results['password'];
+        if(password_verify($password, $passwordHash))
+        {
+            $_SESSION['LOGGED_IN_USER'] = $email;
+            $_SESSION['email'] = $userId;
+            return true;
+        }
+        
+    }
+    static function currentUser()
+    {
+        return $_SESSION['LOGGED_IN_USER'];
+    }
+    static function checkUser()
+    {
+        return !empty($_SESSION['LOGGED_IN_USER']) && isset($_SESSION['LOGGED_IN_USER']) ? true : false;
+    }
+    static function logoutUser()
+    {
+        $_SESSION = array();
+        if(ini_get("session.use_cookies"))
+        {
+        $params = session_get_cookie_params();
+        setcookie(session_name(), '', time() - 42000,
+            $params["path"], $params["domain"],
+            $params["secure"], $params["httponly"]);
+        }
+        session_destroy();
+    }
+}
